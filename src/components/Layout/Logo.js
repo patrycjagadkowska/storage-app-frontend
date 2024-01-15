a23c1700826f5e14aa25c3e6c7f1394c3fd0adff
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-01-15 14:53:06 +0000

    change link path to prop

--- src/components/Layout/Logo.js
+++ src/components/Layout/Logo.js
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom";
 
 import classes from "./styles/Header.module.css";
 
-const Logo = () => {
+const Logo = ({ home }) => {
     return (
         <div className={classes.logo}>
-          <NavLink to="/">
+          <NavLink to={home}>
             <picture>
               <source media="(min-width:768px)" srcSet="logo.png" />
               <img src="logo_sm.png" alt="logo" />
