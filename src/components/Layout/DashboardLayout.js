0b69a2a32d3d7da6856f0aacdcaf71bf6c2b115f
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-01-16 13:16:57 +0000

    add main dashboard layout

---
+++ src/components/Layout/DashboardLayout.js
@@ -0,0 +1,63 @@
+import { Outlet } from "react-router";
+import { FaWarehouse, FaPowerOff } from "react-icons/fa";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdPointOfSale } from "react-icons/md";
+import { IoMdContact, IoMdSettings } from "react-icons/io";
+
+import Header from "./Header";
+import DashboardFooter from "./DashboardFooter";
+
+const DashboardLayout = () => {
+    const logout = () => {};
+    
+    const headerLinks = [
+        {
+            type: "svg-link",
+            path: "/dashboard/stock",
+            text: "stock",
+            svg: <FaWarehouse />
+        },
+        {
+            type: "svg-link",
+            path: "/dashboard/supplies",
+            text: "supplies",
+            svg: <TbTruckDelivery />
+        },
+        {
+            type: "svg-link",
+            path: "/dashboard/sales",
+            text: "sales",
+            svg: <MdPointOfSale />
+        },
+        {
+            type: "svg-link",
+            path: "/dashboard/contacts",
+            text: "contacts",
+            svg: <IoMdContact />
+        },
+        {
+            type: "svg-link",
+            path: "/dashboard/settings",
+            text: "settings",
+            svg: <IoMdSettings />
+        },
+        {
+            type: "svg-button",
+            text: "logout",
+            svg: <FaPowerOff />,
+            fn: logout
+        }
+    ];
+
+    return (
+        <>
+            <Header home="/dashboard" links={headerLinks} dashboard={true} breakpoint={576} />
+            <main>
+                { Outlet }
+            </main>
+            <DashboardFooter />
+        </>
+    );
+};
+
+export default DashboardLayout;
