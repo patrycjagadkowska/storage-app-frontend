25992252b102ac313aa8a3ad4646ef8ce0a3a234
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-01-26 15:35:17 +0000

    add select and add option button

--- src/components/UI/CustomForm.js
+++ src/components/UI/CustomForm.js
@@ -6,80 +6,109 @@ import CustomButton from "../UI/CustomButton";
 import classes from "./styles/CustomForm.module.css";
 
 const CustomForm = ({ inputs, onSubmit, button }) => {
-    const [ customInputs, setCustomInputs ] = useState([]);
-    const [ formValues, setFormValues ] = useState({});
-    const [ formErrors, setFormErrors ] = useState({});
+  const [customInputs, setCustomInputs] = useState([]);
+  const [formValues, setFormValues] = useState({});
+  const [formErrors, setFormErrors] = useState({});
 
-    const getInputValue = useCallback(({ name, value }) => {
-        setFormValues((values) => {
-            const copiedValues = { ...values };
-            copiedValues[name] = value;
-            return copiedValues;
-        });
-    }, [setFormValues]);
+  const getInputValue = useCallback(
+    ({ name, value }) => {
+      setFormValues((values) => {
+        const copiedValues = { ...values };
+        copiedValues[name] = value;
+        return copiedValues;
+      });
+    },
+    [setFormValues]
+  );
 
-    const getError = useCallback(({ name, error }) => {
-        setFormErrors((errors) => {
-            const copiedValues = { ...errors };
-            copiedValues[name] = error ? true : false;
-            return copiedValues;
-        });
-    }, [setFormErrors]);
+  const getError = useCallback(
+    ({ name, error }) => {
+      setFormErrors((errors) => {
+        const copiedValues = { ...errors };
+        copiedValues[name] = error ? true : false;
+        return copiedValues;
+      });
+    },
+    [setFormErrors]
+  );
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        const errors = Object.values(formErrors);
-        const hasError = errors.find(value => value === true);
-        if (hasError) {
-            console.log("I will not submit");
-            return;
-        }
-        console.log("Submitted!");
-        onSubmit(formValues);
-    };
-   
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const errors = Object.values(formErrors);
+    const hasError = errors.find((value) => value === true);
+    if (hasError) {
+      console.log("I will not submit");
+      return;
+    }
+    console.log("Submitted!");
+    onSubmit(formValues);
+  };
 
-    useEffect(() => {
-        let mappedInputs;
-        if (inputs && Array.isArray(inputs) && inputs.length > 0) {
-            mappedInputs = inputs.map((input) => {
-                const { label, id, name, type, initialValue, validationFn, placeholder } = input;
-                let fn;
-                //pass additional data from state to validationFn
-                if (name === "repeat-pass") {
-                    fn = { validationFn, data: formValues.password };
-                } else if (name === "repeatPass") {
-                    fn = { validationFn, data: formValues.newPass };
-                } else {
-                    fn = {validationFn};
-                }
-                return (
-                  <CustomInput
-                    label={label}
-                    id={id}
-                    name={name}
-                    type={type}
-                    key={id}
-                    initialValue={initialValue}
-                    validationFn={fn}
-                    getInputValue={getInputValue}
-                    getError={getError}
-                    placeholder={placeholder}
-                  />
-                );
-            });
+  useEffect(() => {
+    let mappedInputs;
+    if (inputs && Array.isArray(inputs) && inputs.length > 0) {
+      mappedInputs = inputs.map((input) => {
+        const {
+          label,
+          id,
+          name,
+          type,
+          initialValue,
+          validationFn,
+          placeholder,
+          options,
+          addOption,
+        } = input;
+        let fn;
+        //pass additional data from state to validationFn
+        if (name === "repeat-pass") {
+          fn = { validationFn, data: formValues.password };
+        } else if (name === "repeatPass") {
+          fn = { validationFn, data: formValues.newPass };
         } else {
-            mappedInputs = [];
+          fn = { validationFn };
         }
-        setCustomInputs(mappedInputs);
-    }, [inputs, getInputValue, getError, formValues.password, formValues.newPass]);
+        return (
+          <div className={classes["form__row"]}>
+            <CustomInput
+              label={label}
+              id={id}
+              name={name}
+              type={type}
+              key={id}
+              initialValue={initialValue}
+              validationFn={fn}
+              getInputValue={getInputValue}
+              getError={getError}
+              placeholder={placeholder}
+              options={options}
+            />
+            {addOption && (
+              <CustomButton onClick={addOption.handler}>
+                {addOption.icon} <span>{addOption.button}</span>
+              </CustomButton>
+            )}
+          </div>
+        );
+      });
+    } else {
+      mappedInputs = [];
+    }
+    setCustomInputs(mappedInputs);
+  }, [
+    inputs,
+    getInputValue,
+    getError,
+    formValues.password,
+    formValues.newPass,
+  ]);
 
-    return (
-        <form className={classes.form} onSubmit={submitHandler}>
-            { Array.isArray(customInputs) && customInputs.length > 0 && customInputs }
-            <CustomButton>{ button }</CustomButton>
-        </form>
-    );
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      {Array.isArray(customInputs) && customInputs.length > 0 && customInputs}
+      <CustomButton>{button}</CustomButton>
+    </form>
+  );
 };
 
 export default CustomForm;
