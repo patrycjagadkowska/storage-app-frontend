30aa902beed0a6407e481069364804d2901f2d74
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-02-09 09:19:12 +0000

    add sales form

---
+++ src/components/Sales/SalesForm.js
@@ -0,0 +1,222 @@
+import { useState } from "react";
+import { MdOutlinePlaylistAdd } from "react-icons/md";
+import { useNavigate } from "react-router";
+
+import CustomForm from "../UI/CustomForm";
+import CustomButton from "../UI/CustomButton";
+import ItemsList from "../UI/ItemsList";
+import {
+  validateDate,
+  validateSelect,
+  validatePrice,
+  validateQuantity,
+} from "../../constants/validationFns";
+
+const SalesForm = ({ contacts, categories, items, getFormValues }) => {
+    const [ formError, setFormError ] = useState(null);
+    const [ firstFormFilled, setFirstFormFilled ] = useState(false);
+    const [ formValues, setFormValues ] = useState({
+        date: "",
+        customer: "",
+        items: []
+    });
+    const navigate = useNavigate();
+
+    const customerOptions = contacts.map((c) => c.name);
+    const categoriesOptions = categories.map((c) => c.name);
+    const itemsOptions = items.map((i) => i.name);
+
+    const backToFirstForm = () => {
+        setFirstFormFilled(false);
+    };
+
+    const firstInputs = [
+        {
+            type: "date",
+            name: "date",
+            id: "date",
+            label: "Date",
+            validationFn: validateDate,
+            initialValue: formValues.date
+        },
+        {
+            type: "select",
+            name: "customer",
+            id: "customer",
+            label: "Customer",
+            validationFn: validateSelect,
+            initialValue: formValues.customer,
+            options: customerOptions,
+            addOption: {
+                icon: <MdOutlinePlaylistAdd />,
+                button: "Add customer",
+                handler: () => {}
+            }
+        }
+    ];
+
+    const firstFormHandler = (inputValues) => {
+        if (!inputValues.date || inputValues.date === "") {
+            setFormError("Please choose a date.");
+            return;
+        }
+        if (!inputValues.customer || inputValues.customer === "") {
+            setFormError("Please choose a customer from a list or add a new one.");
+            return;
+        }
+
+        setFormValues((oldValues) => {
+            return {...oldValues, date: inputValues.date, customer: inputValues.customer};
+        });
+
+        setFirstFormFilled(true);
+    };
+
+    const secondInputs = [
+        {
+            label: "Category",
+            name: "category",
+            id: "category",
+            type: "select",
+            options: categoriesOptions,
+            validationFn: validateSelect,
+            initialValue: ""
+        },
+        {
+            label: "Item name",
+            name: "name",
+            id: "name",
+            type: "select",
+            options: itemsOptions,
+            validationFn: validateSelect,
+            initialValue: ""
+        },
+        {
+            label: "Price",
+            name: "price",
+            id: "price",
+            type: "text",
+            validationFn: validatePrice,
+            initialValue: ""
+        }, 
+        {
+            label: "Quantity",
+            name: "quantity",
+            id: "quantity",
+            type: "number",
+            validationFn: validateQuantity,
+            initialValue: 0
+        }
+    ];
+
+    const secondFormHandler = (inputValues) => {
+        setFormError(null);
+        if (!inputValues.category || inputValues.category === "") {
+            setFormError("Please choose a category.");
+            return;
+        }
+
+        if (!inputValues.name || inputValues.name === "") {
+            setFormError("Please choose an item name.");
+            return;
+        }
+
+        if (!inputValues.quantity || inputValues.quantity === 0) {
+            setFormError("Please type in item's quantity.");
+            return;
+        }
+
+        if (!inputValues.price || inputValues.price === "") {
+            setFormError("Please type in item's price.");
+            return;
+        }
+
+        setFormValues((oldValues) => {
+            const itemsArray = [...oldValues.items];
+            itemsArray.push(inputValues);
+            return {...oldValues, items: itemsArray};
+        });
+    };
+
+    const formSubmitHandler = async (event) => {
+        event.preventDefault();
+
+        if (formError) {
+            return;
+        }
+
+        if (formValues.length === 0) {
+            setFormError("Please add any items.");
+        }
+
+        try {
+            const token = localStorage.getItem("token");
+
+            const customerId = contacts.find((c) => c.name === formValues.customer).id;
+            const mappedItems = formValues.items.map((i) => {
+                return {
+                    categoryName: i.category,
+                    itemName: i.name,
+                    price: i.price,
+                    quantity: i.quantity
+                };
+            });
+            
+            const res = await fetch("http://localhost:8080/addSale", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token
+                },
+                body: JSON.stringify({
+                    customerId, 
+                    date: formValues.date,
+                    items: mappedItems
+                })
+            });
+
+            if (res.status === 200 || res.status === 201) {
+                navigate(0);
+            } else {
+                console.log("There was a problem while adding data");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+      <>
+        {!firstFormFilled && (
+          <CustomForm
+            inputs={firstInputs}
+            onSubmit={firstFormHandler}
+            button="Next"
+            formError={formError}
+          />
+        )}
+        {firstFormFilled && (
+          <CustomForm
+            inputs={secondInputs}
+            onSubmit={secondFormHandler}
+            button="Add item"
+            getFormValues={getFormValues}
+            formError={formError}
+          />
+        )}
+        {firstFormFilled && <ItemsList itemsArray={formValues.items} />}
+        {firstFormFilled && (
+          <CustomButton onClick={backToFirstForm} type="button">
+            Back
+          </CustomButton>
+        )}
+        {firstFormFilled && (
+          <CustomButton type="submit" onClick={formSubmitHandler}>
+            Add sale
+          </CustomButton>
+        )}
+      </>
+    );
+};
+
+export default SalesForm;
--- src/pages/Dashboard/Sales.js
+++ src/pages/Dashboard/Sales.js
@@ -1,23 +1,95 @@
 import { useLoaderData } from "react-router";
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useCallback } from "react";
 
 import { fetchData } from "../../constants/helperFns";
+import HeaderWithButtons from "../../components/UI/HeaderWithButons";
+import SalesForm from "../../components/Sales/SalesForm";
 
 const Sales = () => {
     const [ sales, setSales ] = useState([]);
-    const loadedSales = useLoaderData();
+    const [ categories, setCategories ] = useState([]);
+    const [ contacts, setContacts ] = useState([]);
+    const [ items, setItems ] = useState([]);
+    const [ showForm, setShowForm ] = useState(false);
+    const [ chosenCategory, setChosenCategory ] = useState(null);
+    const loadedData = useLoaderData();
 
     useEffect(() => {
-        if (!loadedSales || !Array.isArray(loadedSales) || loadedSales.length === 0) {
-            setSales([]);
+        if (!loadedData.sales || !Array.isArray(loadedData.sales) || loadedData.sales.length === 0) {
+          setSales([]);
+        } else {
+          setSales(loadedData.sales);
         }
 
-        setSales(loadedSales.sales);
-    }, [loadedSales]);
+        if (!loadedData.categories || !Array.isArray(loadedData.categories) || loadedData.categories.length === 0) {
+          setCategories([]);
+        } else {
+          setCategories(loadedData.categories);
+        }
+
+        if (!loadedData.contacts || !Array.isArray(loadedData.contacts) || loadedData.contacts.length === 0) {
+          setContacts([]);
+        } else {
+          setContacts(loadedData.contacts);
+        }
+    }, [loadedData]);
+
+    const getFormValues = useCallback((formValues) => {
+      setChosenCategory(formValues.category);
+    }, []);
+
+    useEffect(() => {
+      if (!chosenCategory || chosenCategory === "") {
+        return;
+      }
+
+      const categoryId = categories.find((c) => c.name === chosenCategory).id;
+      
+      if (!categoryId) {
+        console.log("Id not found");
+        return;
+      }
+
+      const fetchItems = async () => {
+        const token = localStorage.getItem("token");
+
+        try {
+          const res = await fetchData(token, "http://localhost:8080/categories/" + categoryId);
+          setItems(res.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      fetchItems();
+    }, [chosenCategory, categories]);
+
+    const listButton = {
+        handler: () => {setShowForm(false)},
+        text: "Show all sales"
+    };
+
+    const formButton = {
+        handler: () => {setShowForm(true)},
+        text: "Add sale"
+    };
 
     return (
-        <h1>Sales</h1>
+      <>
+        <HeaderWithButtons
+          header="Sales"
+          listButton={listButton}
+          formButton={formButton}
+        />
+        {showForm && (
+          <SalesForm
+            categories={categories}
+            contacts={contacts}
+            getFormValues={getFormValues}
+            items={items}
+          />
+        )}
+      </>
     );
 
 };
@@ -26,5 +98,22 @@ export default Sales;
 
 export const loader = async () => {
     const token = localStorage.getItem("token");
-    return fetchData(token, "http://localhost:8080/sales");
+    const contactsUrl = "http://localhost:8080/contacts";
+    const salesUrl = "http://localhost:8080/sales";
+    const categoriesUrl = "http://localhost:8080/categories";
+    try {
+      const allData = await Promise.all([
+        fetchData(token, contactsUrl),
+        fetchData(token, salesUrl),
+        fetchData(token, categoriesUrl)
+      ]);
+
+      return {
+        contacts: allData[0].data,
+        sales: allData[1].data,
+        categories: allData[2].data
+      };
+    } catch (error) {
+      console.log(error);
+    }
 };
