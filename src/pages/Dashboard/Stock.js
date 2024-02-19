071734c976ba4a8dd3a1e91bd7ec86e5dc65692e
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-02-19 15:19:44 +0000

    add stock list, fetch category items after filter

--- src/pages/Dashboard/Stock.js
+++ src/pages/Dashboard/Stock.js
@@ -1,11 +1,15 @@
 import { useLoaderData } from "react-router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 import { fetchData } from "../../constants/helperFns";
+import HeaderWithButtons from "../../components/UI/HeaderWithButons";
+import StockList from "../../components/Stock/StockList";
 
 const Stock = () => {
+    const [ showForm, setShowForm ] = useState(false);
     const [ categories, setCategories ] = useState();
     const [ items, setItems ] = useState();
+    const [ chosenCategory, setChosenCategory ] = useState(null);
     const loadedData = useLoaderData();
 
     useEffect(() => {
@@ -17,9 +21,68 @@ const Stock = () => {
             setItems(loadedData.items);
         }
     }, [loadedData]);
-    
+
+    useEffect(() => {
+        const category =
+          (categories &&
+            categories.find((c) => c.name === chosenCategory)) ||
+          null;
+        const token = localStorage.getItem("token");
+        const fetchCategoryItems = async () => {
+            const res = await fetch("http://localhost:8080/categories/" + category.id, {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            });
+
+            if (res.status === 200) {
+                const items = await res.json();
+                setItems(items.data);
+            }
+        };
+
+        if (category && category.id) {
+            fetchCategoryItems();
+        }
+    }, [chosenCategory, categories]);
+
+    const listButton = {
+        handler: () => {setShowForm(false)},
+        text: "Show all stock"
+    };
+
+    const formButton = {
+        handler: () => {setShowForm(true)},
+        text: "Take inventory"
+    };
+
+    const onFilterChangeHandler = useCallback(({ name, value }) => {
+        if (name === "category") {
+            setChosenCategory(value);
+        }
+    }, []);
+
+    const onErrorHandler = useCallback(({ name, value }) => {
+
+    }, []);
+
     return (
-        <h1>Stock</h1>
+      <>
+        <HeaderWithButtons
+          header="Stock"
+          listButton={listButton}
+          formButton={formButton}
+        />
+        {!showForm && (
+          <StockList
+            onChangeHandler={onFilterChangeHandler}
+            items={items}
+            categories={categories}
+            onErrorHandler={onErrorHandler}
+          />
+        )}
+      </>
     );
 };
 
