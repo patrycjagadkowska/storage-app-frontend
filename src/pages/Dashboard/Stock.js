350388069f1c4aa3e95fc2aba7a2970c3206cea4
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-02-19 13:09:33 +0000

    add loader, set state with loaded data

--- src/pages/Dashboard/Stock.js
+++ src/pages/Dashboard/Stock.js
@@ -1,4 +1,23 @@
+import { useLoaderData } from "react-router";
+import { useState, useEffect } from "react";
+
+import { fetchData } from "../../constants/helperFns";
+
 const Stock = () => {
+    const [ categories, setCategories ] = useState();
+    const [ items, setItems ] = useState();
+    const loadedData = useLoaderData();
+
+    useEffect(() => {
+        if (loadedData && loadedData.categories && Array.isArray(loadedData.categories)) {
+            setCategories(loadedData.categories);
+        } 
+
+        if (loadedData && loadedData.items && Array.isArray(loadedData.items)) {
+            setItems(loadedData.items);
+        }
+    }, [loadedData]);
+    
     return (
         <h1>Stock</h1>
     );
@@ -7,5 +26,21 @@ const Stock = () => {
 export default Stock;
 
 export const loader = async () => {
-    return null;
+    const token = localStorage.getItem("token");
+    const categoriesUrl = "http://localhost:8080/categories";
+    const itemsUrl = "http://localhost:8080/categories/items";
+
+    try {
+        const allData = await Promise.all([
+            fetchData(token, categoriesUrl),
+            fetchData(token, itemsUrl)
+        ]);
+
+        return {
+            categories: allData[0].data,
+            items: allData[1].data
+        };
+    } catch (error) {
+        console.log(error);
+    }
 };
