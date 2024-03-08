eea55e097cabedf1d9f232faf4eba5bfa6a8e65f
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-03-08 13:55:15 +0000

    add SaleItemsList and table classes

---
+++ src/components/Sales/SaleItemsList.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+import classes from "../UI/styles/CustomTable.module.css";
+
+const SaleItemsList = ({ items, categories }) => {
+    const [ itemsRows, setItemsRows ] = useState([]);
+
+    useEffect(() => {
+        if (!items || !Array.isArray || items.length <= 0) {
+            setItemsRows([]);
+            return;
+        }
+
+        const mappedRows = items.map((i) => {
+            const categoryName = categories.find((c) => c.id === i.CategoryId).name;
+            return (
+                <tr>
+                    <td>{i.name}</td>
+                    <td>{categoryName}</td>
+                    <td>{i.SaleItem.quantity}</td>
+                    <td>{i.SaleItem.price}</td>
+                </tr>
+            );
+        });
+        setItemsRows(mappedRows);
+    }, [items, categories]);
+
+    return (
+        <table className={classes.table}>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {itemsRows}
+            </tbody>
+        </table>
+    );
+};
+
+export default SaleItemsList;
--- src/components/Sales/SalesList.js
+++ src/components/Sales/SalesList.js
@@ -3,8 +3,10 @@ import { RiDeleteBinFill } from "react-icons/ri";
 import { FaEdit } from "react-icons/fa";
 
 import CustomList from "../UI/CustomList";
-import ItemsList from "../UI/ItemsList";
 import CustomButton from "../UI/CustomButton";
+import SaleItemsList from "./SaleItemsList";
+
+import classes from "../UI/styles/ComplexList.module.css";
 
 const SalesList = ({ sales, contacts, categories, openDeleteModal, openEditModal }) => {
     const [ salesList, setSalesList ] = useState([]);
@@ -17,21 +19,30 @@ const SalesList = ({ sales, contacts, categories, openDeleteModal, openEditModal
 
         const mappedSales = sales.map((sale) => {
             const customer = contacts.find((c) => c.id === sale.ContactId).name;
-            const saleItems = sale.Items.map((i) => {
-                const categoryName = categories.find((c) => c.id === i.CategoryId).name;
-                return {
-                    name: i.name,
-                    category: categoryName,
-                    price: i.SaleItem.price,
-                    quantity: i.SaleItem.quantity
-                };
-            });
             return {
-                title: <div><span>{sale.date + " - " + customer}</span><div>
-                    <CustomButton onClick={() => openEditModal(sale.id)}><FaEdit /> <span>Edit</span></CustomButton>
-                    <CustomButton onClick={() => openDeleteModal(sale.id)}><RiDeleteBinFill /> <span>Delete</span></CustomButton></div></div>,
-                content: <ItemsList title="Sold items" itemsArray={saleItems} />
-            }
+              title: (
+                <div className={classes["list-header"]}>
+                  <span>{sale.date + " - " + customer}</span>
+                  <div className={classes["list-header__handlers"]}>
+                    <CustomButton
+                      className={classes["list-header__handler"]}
+                      onClick={() => openEditModal(sale.id)}
+                    >
+                      <FaEdit /> <span>Edit</span>
+                    </CustomButton>
+                    <CustomButton
+                      className={classes["list-header__handler"]}
+                      onClick={() => openDeleteModal(sale.id)}
+                    >
+                      <RiDeleteBinFill /> <span>Delete</span>
+                    </CustomButton>
+                  </div>
+                </div>
+              ),
+              content: (
+                <SaleItemsList items={sale.Items} categories={categories} />
+              ),
+            };
         });
 
         setSalesList(mappedSales);
