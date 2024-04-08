0f5d8c018e0e977a2510d6cb548bd4e09dbd0081
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-04-08 15:27:44 +0100

    create MonthlyIncome component

---
+++ src/components/Dashboard/MonthlyIncome.js
@@ -0,0 +1,34 @@
+import StatCard from "./StatCard";
+
+import classes from "./Dashboard.module.css";
+
+const MonthlyIncome = ({ data }) => {
+    return (
+      <StatCard>
+        <h3>Monthly Income</h3>
+        <div className={classes["financial-summary"]}>
+          <div className={classes["financial-summary__col"]}>
+            <span>Sales:</span>
+            <span>Expenses:</span>
+          </div>
+          <div className={classes["financial-summary__col"]}>
+            <span>{data.totalSales}</span>
+            <span>{data.totalExpenses}</span>
+          </div>
+          <div className={classes["vertical-line"]} />
+          <div className={classes["financial-summary__col"]}>
+            <span>Profit Margin:</span>
+          </div>
+          <div className={classes["financial-summary__col"]}>
+            <span>{data.profitMargin}</span>
+          </div>
+        </div>
+        <div className={classes["warehouse__summary"]}>
+          <h4>Warehouse Value:</h4>
+          <span>{data.warehouseValue}</span>
+        </div>
+      </StatCard>
+    );
+};
+
+export default MonthlyIncome;
