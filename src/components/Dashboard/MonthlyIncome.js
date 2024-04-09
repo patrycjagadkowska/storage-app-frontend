fd0ae9066e615f6e1b972a1bbd8bcdf1f6017640
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-04-09 14:02:09 +0100

    add styles to  dashboard page

--- src/components/Dashboard/MonthlyIncome.js
+++ src/components/Dashboard/MonthlyIncome.js
@@ -5,7 +5,7 @@ import classes from "./Dashboard.module.css";
 const MonthlyIncome = ({ data }) => {
     return (
       <StatCard>
-        <h3>Monthly Income</h3>
+        <h3 className={classes.header}>Monthly Income</h3>
         <div className={classes["financial-summary"]}>
           <div className={classes["financial-summary__col"]}>
             <span>Sales:</span>
@@ -23,7 +23,7 @@ const MonthlyIncome = ({ data }) => {
             <span>{data.profitMargin}</span>
           </div>
         </div>
-        <div className={classes["warehouse__summary"]}>
+        <div className={classes["warehouse-summary"]}>
           <h4>Warehouse Value:</h4>
           <span>{data.warehouseValue}</span>
         </div>
