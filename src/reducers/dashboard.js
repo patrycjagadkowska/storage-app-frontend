5ad00afc80a6e3dd9bbbf1106edb31f962f85de8
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-04-16 14:39:32 +0100

    add sixMonthsData to state and reducer

--- src/reducers/dashboard.js
+++ src/reducers/dashboard.js
@@ -1,14 +1,33 @@
 export const initState = {
-    monthlyIncomeData: []
+    monthlyIncomeData: {},
+    sixMonthsData: [],
 };
 
 export const reducer = (state, action) => {
     switch (action.type) {
         case "set_monthly_income_data": {
+            if (!action.data || typeof action.data !== "object") {
+                return {
+                    ...state, 
+                    monthlyIncomeData: {}
+                };
+            }
             return {
                 ...state, 
                 monthlyIncomeData: action.data
             };
+        }
+        case "set_six_months_data": {
+            if (!action.data || !Array.isArray(action.data)) {
+                return {
+                    ...state, 
+                    sixMonthsData: action.data
+                };
+            }
+            return {
+                ...state,
+                sixMonthsData: action.data
+            }
         }
         default: {
             return state;
