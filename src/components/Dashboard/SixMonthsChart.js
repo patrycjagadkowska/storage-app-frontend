de45b48842f51625ad9b60abd7917e3e9121701c
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-04-16 14:39:52 +0100

    add chart with six months data

---
+++ src/components/Dashboard/SixMonthsChart.js
@@ -0,0 +1,53 @@
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
+} from "recharts";
+
+import classes from "./Dashboard.module.css";
+
+const SixMonthsChart = ({ data }) => {
+    const deviceWidth = window.screen.width;
+    const width = deviceWidth < 992 ? deviceWidth *0.8 : deviceWidth * 0.5;
+    const hide = deviceWidth < 992 ? true : false;
+   
+  return (
+    <section className={classes.chart}>
+      <AreaChart width={width} height={200} data={data}>
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" hide={hide} />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="Sales"
+          stroke="#8884d8"
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+        <Area
+          type="monotone"
+          dataKey="Expenses"
+          stroke="#82ca9d"
+          fillOpacity={1}
+          fill="url(#colorPv)"
+        />
+      </AreaChart>
+    </section>
+  );
+};
+
+export default SixMonthsChart;
