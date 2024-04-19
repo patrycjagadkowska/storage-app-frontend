1cb9aeaba82e3837593d3a1eb24a32c1ecb1266a
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-04-19 14:10:10 +0100

    add Summary and fetch itemsSummary fn

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import MonthlyIncome from "../../components/Dashboard/MonthlyIncome";
 import { fetchData } from "../../constants/helperFns";
 import SixMonthsChart from "../../components/Dashboard/SixMonthsChart";
 import MonthPieChart from "../../components/Dashboard/MonthPieChart";
+import Summary from "../../components/Dashboard/Summary";
 
 import classes from "./pages.module.css";
 
@@ -14,18 +15,17 @@ const Dashboard = () => {
     const loadedData = useLoaderData();
 
     useEffect(() => {
-        if (!loadedData.data || !Array.isArray(loadedData.data) || loadedData.data.length < 3) {
+        if (!loadedData.data || !Array.isArray(loadedData.data) || loadedData.data.length < 4) {
             dispatch({ type: "set_monthly_income_data", data: {} });
             dispatch({ type: "set_six_months_data", data: [] });
             dispatch({ type: "set_last_month_data", data: [] });
-
+            dispatch({ type: "set_items_summary", data: {} });
         }
         dispatch({ type: "set_monthly_income_data", data: loadedData[0].data });
         dispatch({ type: "set_six_months_data", data: loadedData[1].data });
         dispatch({ type: "set_last_month_data", data: loadedData[2].data });
+        dispatch({ type: "set_items_summary", data: loadedData[3].data });
     }, [loadedData]);
-
-    console.log(state.lastMonthData);
 
     return (
         <div className={classes.container}>
@@ -33,6 +33,7 @@ const Dashboard = () => {
         <MonthlyIncome data={state.monthlyIncomeData} />
         <SixMonthsChart data={state.sixMonthsData} />
         <MonthPieChart title="Monthly sales" data={state.lastMonthData} />
+        <Summary data={state.itemsSummary} />
         </div>
     );
 };
@@ -50,11 +51,14 @@ export const loader = async () => {
       "http://localhost:8080/last-six-months?year=" + year + "&month=" + month;
       const lastMonthUrl =
         "http://localhost:8080/last-month?year=" + year + "&month=" + month;
+    const itemsSummaryUrl =
+      "http://localhost:8080/items-summary?year=" + year + "&month=" + month;
 
     const allData = await Promise.all([
         fetchData(token, monthlyIncomeUrl),
         fetchData(token, lastSixMonthsUrl),
-        fetchData(token, lastMonthUrl)
+        fetchData(token, lastMonthUrl),
+        fetchData(token, itemsSummaryUrl)
     ]);
 
     return allData;
