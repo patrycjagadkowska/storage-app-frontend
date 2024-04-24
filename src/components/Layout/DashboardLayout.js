3607ec5835ca501d80e1e99b1e87875c2592fec9
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2024-04-24 11:17:40 +0100

    add loggedIn validation

--- src/components/Layout/DashboardLayout.js
+++ src/components/Layout/DashboardLayout.js
@@ -2,7 +2,7 @@ import { FaWarehouse, FaPowerOff } from "react-icons/fa";
 import { TbTruckDelivery } from "react-icons/tb";
 import { MdPointOfSale } from "react-icons/md";
 import { IoMdContact, IoMdSettings } from "react-icons/io";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 import Header from "./Header";
@@ -11,8 +11,14 @@ import DashboardMain from "./DashboardMain";
 import { AuthContext } from "../../context/auth-context";
 
 const DashboardLayout = () => {
-    const { logout } = useContext(AuthContext);
+    const { logout, isLoggedIn } = useContext(AuthContext);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            navigate("/login");
+        }
+    }, [isLoggedIn, navigate]);
 
     const logoutHandler = () => {
         logout();
